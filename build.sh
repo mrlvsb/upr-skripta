0caab9547d3d13c4c45ebf73cc7c57e0466edd23
Daniel Trnka <daniel.trnka@gmail.com>
2020-09-09 10:53:07 +0200

    Remove trailing newline from code so they are not shown in ace.js

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 # cargo install mdbook
 
 mdbook build
+
+find book -name "*.html" | while read -r file; do
+  perl -i -pe 'BEGIN{undef $/;} s/\n*<\/code>/<\/code>/smg' "$file"
+done
+
 # mdbook serve
